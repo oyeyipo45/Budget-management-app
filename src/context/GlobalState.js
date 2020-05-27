f28399b6d18e98ce2bfe11b4f1bf4fd6e40dfb44
oyeyipo45 <oyeyipo45@gmail.com>
2020-05-27 12:46:17 +0100

    delete transaction done

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -30,9 +30,19 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-
+    
+  function deleteTransaction(id){
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider 
+    value={{ deleteTransaction , 
+      transactions: state.transactions
+            
+    }}> 
       {children}
     </GlobalContext.Provider>
   );
