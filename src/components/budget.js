c0b81e09ced01b54b061cc89b183ea945d4bbe62
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-03 01:44:07 +0100

    budget totaled

--- src/components/budget.js
+++ src/components/budget.js
@@ -3,18 +3,19 @@ import { GlobalContext } from "../context/GlobalState";
 
 
 export const Budget = () => {
-    const  { transactions } = useContext(GlobalContext);
+    const  { transactions, budget } = useContext(GlobalContext);
 
-
+  console.log(budget);
+  
     // const amounts = transactions.map(transaction => transaction.amount);
-    // const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
-    
+     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+      const budget = 500 - total;
     return (
       <React.Fragment>
         <div className="inc-exp-container">
           <div>
             <h4>Budget</h4>
-            <h4>$4000000.00</h4>
+            <h4>{budget}</h4>
           </div>
           <div>
             <h4>Your Balance</h4>
