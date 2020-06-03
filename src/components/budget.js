97af796e4c685bc587beb7674b1ba90e577effe1
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-03 01:27:18 +0100

    budget component setup

---
+++ src/components/budget.js
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react'
+import { GlobalContext } from "../context/GlobalState";
+
+
+export const Budget = () => {
+    const  { transactions } = useContext(GlobalContext);
+
+
+    // const amounts = transactions.map(transaction => transaction.amount);
+    // const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+    
+    return (
+      <React.Fragment>
+        <div className="inc-exp-container">
+          <div>
+            <h4>Budget</h4>
+            <h4>$4000000.00</h4>
+          </div>
+          <div>
+            <h4>Your Balance</h4>
+            <h4>$200.20</h4>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+}
